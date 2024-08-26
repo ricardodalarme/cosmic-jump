a4af9cfbafd3c863327f5c55366980770936eebb
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-08-26 19:51:47 -0300

    refactor: move screen size to the root

---
+++ lib/utils/screen_size.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const double tileSize = 16;
+const double numTilesX = 22;
+const double numTilesY = 40;
+
+const double screenWidth = tileSize * numTilesX;
+const double screenHeight = tileSize * numTilesY;
+
+const Size screenSize = Size(screenWidth, screenHeight);
