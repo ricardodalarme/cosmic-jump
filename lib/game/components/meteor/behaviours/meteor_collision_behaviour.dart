05d8fa516ab05c69c28aee8eaf032ad3900869a3
ricardodalarme <ricardodalarme@outlook.com>
2024-08-27 21:46:04 -0300

    feat(meteor): explde meteor when it collides with other meteor

--- lib/game/components/meteor/behaviours/meteor_collision_behaviour.dart
+++ lib/game/components/meteor/behaviours/meteor_collision_behaviour.dart
@@ -4,8 +4,11 @@ import 'package:leap/leap.dart';
 class MeteorCollisionBehavior extends PhysicalBehavior<MeteorComponent> {
   @override
   void update(double dt) {
-    for (final other in collisionInfo.downCollisions) {
+    for (final other in collisionInfo.allCollisions) {
       switch (other) {
+        case MeteorComponent():
+          parent.explode();
+          other.explode();
         case LeapMapGroundTile(:final tile) when tile.class_ == 'TheGround':
           parent.explode();
         default:
